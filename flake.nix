13467e4b5b9985c8a9aac97ec53b1cc93bf215ca
Glen Ernstrom <glenernstro@gmail.com>
2025-10-02 19:24:46 -0400

    first commit on work PC

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "System configuration flake!";
+
+  inputs = {
+     nixpkgs.url = "nixpkgs/nixos-unstable";
+     home-manager.url = "github:nix-community/home-manager/master";
+     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+     nixvim.url ="github:nix-community/nixvim";
+     nixvim.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {self, nixpkgs,...}@inputs:
+   let 
+     lib = nixpkgs.lib;
+     system = "x86_64-linux";
+     pkgs = nixpkgs.legacyPackages.${system};
+   in {
+   nixosConfigurations = {
+      catamount = lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+      };
+     };
+   homeConfigurations."ernstrom" =
+     inputs.home-manager.lib.homeManagerConfiguration {
+       inherit pkgs;
+       modules = [ ./home.nix ];
+       extraSpecialArgs = { inherit inputs; };
+    };
+   };
+ }
